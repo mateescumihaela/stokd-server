ccf9c9bd0c75889fb20812a4f9116e5400109512
Mihaela Mateescu <miha.mateescu@gmail.com>
2020-07-11 12:56:19 +0100

    deployed database

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
@@ -10,7 +12,7 @@ const router = require('./router')
 
 // ************************ connect mongo to mongoose ************************
 
-mongoose.connect(dbURI,
+mongoose.connect(process.env.MONGODB_URI,
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
   () => console.log('Mongo is connected'))
 
